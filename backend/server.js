398edd206664556d8c97b6c1c463d12d74b1c386
Amar Kumar <amarkumarsbg13@gmail.com>
2025-06-08 12:25:16 +0530

    changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,30 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
 dotenv.config();
-connectDB(); // connect to MongoDB
+connectDB();
 
 const app = express();
-app.use(cors());
+
+// ✅ CORS setup: allow only frontend domain
+app.use(
+  cors({
+    origin: "https://appointment-frontend-o73d.onrender.com",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
-// Simple Test Route
+// Test route
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
 const bookingRoutes = require("./routes/bookingRoutes");
 app.use("/api", bookingRoutes);
-// Start Server
+
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
